e1e89033f6b59b704564e8c81c1902ba89823873
Chinchulin-kun <lucakumric@gmail.com>
2022-06-16 21:00:44 -0300

    agrego path y imagenes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
+/* recursos estáticos */
+app.use(express.static('public'))
 
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+/* app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html'))) */
 
 
 
